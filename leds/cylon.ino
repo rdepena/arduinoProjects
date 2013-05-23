574b5d3d56e269e8cdeee86fc097c8c28636aba7
Ricardo De Pena <ricardo.depena@gmail.com>
2013-05-22 23:57:10 -0400

    first arduino project: cylon leds

---
+++ leds/cylon.ino
@@ -0,0 +1,63 @@
+//use some red leds and you got a cylon looking somehting.
+
+// pin 13 is a LED connected to most Arduino boards
+const int led = 13;
+
+//some leds connected to the board:
+const int ledArray[] = {2, 3, 4, 5, 6, 7};
+
+//our button is connected to pin 8
+const int button = 8;
+
+//we soecify the time each led will remain on.
+const int delayTime = 120;
+
+//Array size is going to be the same during execution.
+//size of returns the amount of bytes a variable ocupies, 
+//we need to divide by the size of the variable type to get the length of the array.
+const int arraySize = (sizeof(ledArray) / sizeof(int));
+
+//Setup is a routine that runs once
+void setup () {
+	
+	//Configure the serial or debugging. 
+	Serial.begin(9600);
+
+	//we need to configure the pins
+	pinMode(led, OUTPUT);
+	pinMode(button, INPUT);
+
+	for (int c = 0; c < sizeof(ledArray); c++) {
+		pinMode(ledArray[c], OUTPUT);
+	}
+}
+
+//the loop routine runs over and over forever.
+void loop () {
+	if (digitalRead(button) == 1) {
+
+		//we turn off the internal led
+		digitalWrite(led, LOW);
+		
+		Serial.println(arraySize);
+		//we loop forward.
+		for (int y = 0; y <arraySize; y++) {
+			Serial.println(y);
+			digitalWrite(ledArray[y], HIGH);
+			delay(delayTime);
+			digitalWrite(ledArray[y], LOW);
+		}
+		Serial.println("now down");
+		//we loop backwards
+		for (int x = arraySize -1; x >= 0; x--) {
+			Serial.println(x);
+			digitalWrite(ledArray[x], HIGH);
+	 		delay(delayTime);
+			digitalWrite(ledArray[x], LOW);
+		}
+	}
+	else {
+		//the internal led turns on 
+		digitalWrite(led, HIGH);
+	}
+}
