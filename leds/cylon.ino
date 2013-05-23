d3049523734528686984fbd0b24900cc1165f364
Ricardo de Peña <ricardo.depena@gmail.com>
2013-05-23 01:04:56 -0300

    Update cylon.ino
    formating.

--- leds/cylon.ino
+++ leds/cylon.ino
@@ -9,7 +9,7 @@ const int ledArray[] = {2, 3, 4, 5, 6, 7};
 //our button is connected to pin 8
 const int button = 8;
 
-//we soecify the time each led will remain on.
+//we specify the time each led will remain on.
 const int delayTime = 120;
 
 //Array size is going to be the same during execution.
@@ -20,13 +20,13 @@ const int arraySize = (sizeof(ledArray) / sizeof(int));
 //Setup is a routine that runs once
 void setup () {
 	
-	//Configure the serial or debugging. 
+	//Configure the serial for debugging. 
 	Serial.begin(9600);
 
 	//we need to configure the pins
 	pinMode(led, OUTPUT);
 	pinMode(button, INPUT);
-
+	//initialize each led in the array
 	for (int c = 0; c < sizeof(ledArray); c++) {
 		pinMode(ledArray[c], OUTPUT);
 	}
@@ -34,12 +34,16 @@ void setup () {
 
 //the loop routine runs over and over forever.
 void loop () {
+	
+	//did someone press our button?
 	if (digitalRead(button) == 1) {
 
 		//we turn off the internal led
 		digitalWrite(led, LOW);
 		
+		//debug info
 		Serial.println(arraySize);
+		
 		//we loop forward.
 		for (int y = 0; y <arraySize; y++) {
 			Serial.println(y);
@@ -47,7 +51,10 @@ void loop () {
 			delay(delayTime);
 			digitalWrite(ledArray[y], LOW);
 		}
+		
+		//debug info
 		Serial.println("now down");
+		
 		//we loop backwards
 		for (int x = arraySize -1; x >= 0; x--) {
 			Serial.println(x);
